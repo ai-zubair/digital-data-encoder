985883a6cd48d179f7d859c68ae9c27c6598af35
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-09-01 00:05:07 +0530

    Add the navigation bar for encoder screen

--- src/ts/components/AppWrapper/AppWrapper.ts
+++ src/ts/components/AppWrapper/AppWrapper.ts
@@ -4,7 +4,7 @@ import { AppComponentName, AppEvents, AppAttributes } from '../common/appConstan
 
 class AppWrapper extends CustomElement{
 
-  private activeComponentID: AppComponentName = AppComponentName.LandingBanner;
+  private activeComponentID: AppComponentName = AppComponentName.EncoderScreen;
   private breadCrumb: Element | null = null;
   private componentMap: { [key: string]: Element } = {};
 
--- src/ts/components/EncoderScreen/encoderScreenContent.ts
+++ src/ts/components/EncoderScreen/encoderScreenContent.ts
@@ -10,38 +10,44 @@ const encoderScreenContent = `
       <div slot="tab" tabId="nrzl-encoding" class="tab-title-containers" >
         <button tabId="nrzl-encoding" class="tab-titles">NRZ-L</button>
       </div>
-      <div slot="panel" class="encoding-panel">
-        This is the encoding panel
+      <div slot="panel" class="encoding-panels">
+        This is the encoding panel NRZ-L
       </div>
 
       <div slot="tab" tabId="rz-encoding" class="tab-title-containers">
         <button tabId="rz-encoding" class="tab-titles">RZ</button>
       </div>
-      <div slot="panel" class="encoding-panel">
-        This is the encoding panel
+      <div slot="panel" class="encoding-panels">
+        This is the encoding panel RZ
       </div>
 
       <div slot="tab" tabId="manchester-encoding" class="tab-title-containers">
         <button tabId="manchester-encoding" class="tab-titles">MANCHESTER</button>
       </div>
-      <div slot="panel" class="encoding-panel">
-        This is the encoding panel
+      <div slot="panel" class="encoding-panels">
+        This is the encoding panel MANC
       </div>
 
-      <div slot="tab" tabId="diff-manchester" class="tab-title-containers" active>
+      <div slot="tab" tabId="diff-manchester" class="tab-title-containers">
         <button tabId="diff-manchester" class="tab-titles">D-MANCHESTER</button>
       </div>
-      <div slot="panel" class="encoding-panel">
-        This is the encoding panel
+      <div slot="panel" class="encoding-panels">
+        This is the encoding panel D_MANC
       </div>
 
       <div slot="tab" tabId="ami-encoding" class="tab-title-containers">
         <button tabId="ami-encoding" class="tab-titles">AMI</button>
       </div>
-      <div slot="panel" class="encoding-panel">
-        This is the encoding panel
+      <div slot="panel" class="encoding-panels">
+        This is the encoding panel AMI
       </div>
     </fancy-tabs>
+  </div>
+  <div class="navigation-bar-container">
+    <div class="navigation-bar">
+      <button class="back-button" id="back-button">BACK</button>
+      <a href="https://www.linkedin.com/in/zubair-bashir/" target="_blank" class="profile-link">Like My Work? 👏🏻</a>
+    </div>
   </div>
 `;
 
