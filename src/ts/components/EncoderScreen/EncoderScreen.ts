8b7970a19bc55c3f1a959dfe046875dade479555
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-09-01 00:15:07 +0530

    Wire up back button

--- src/ts/components/EncoderScreen/EncoderScreen.ts
+++ src/ts/components/EncoderScreen/EncoderScreen.ts
@@ -1,9 +1,25 @@
 import { CustomElement } from '../CustomElement/CustomElement';
 import { encoderScreenContent } from './encoderScreenContent';
+import { AppEvents, AppAttributes, AppComponentName } from '../common/appConstants';
 
 class EncoderScreen extends CustomElement{
   constructor() {
     super(encoderScreenContent);
+    this.bindEventListeners();
+  }
+
+  bindEventListeners = (): void => {
+    const backButton = this.shadowRoot?.querySelector("#back-button") as HTMLButtonElement;
+    backButton.addEventListener("click", this.handleBackButtonClick);
+  }
+
+  handleBackButtonClick = (): void => {
+    const activeComponentChangeEvent = new CustomEvent(AppEvents.ActiveComponentChange,{
+      detail:{
+        [AppAttributes.ComponentId]: AppComponentName.StreamBitForm
+      }
+    })
+    this.parentElement?.dispatchEvent(activeComponentChangeEvent);
   }
 }
 
